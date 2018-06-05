f3b3fb7516878ea095efc2c56842a22ac583758a
PinheadLarry1924 <39932381+PinheadLarry1924@users.noreply.github.com>
2018-06-04 19:02:16 -0700

    nipple

---
+++ Pugilism/Player.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+const GRAVITY_VEC = Vector2(0, 900)
+const FLOOR_NORMAL = Vector2(0, -1)
+const SLOPE_SLIDE_STOP = 25.0
+const MIN_ONAIR_TIME = 0.1
+const WALK_SPEED = 250 # pixels/sec
+const JUMP_SPEED = 480
+const SIDING_CHANGE_SPEED = 10
+const BULLET_VELOCITY = 1000
+const SHOOT_TIME_SHOW_WEAPON = 0.2
+
+var linear_vel = Vector2()
+var onair_time = 0 #
+var on_floor = false
+var shoot_time=99999 #time since last shot
+
+var anim=""
+
+#cache the sprite here for fast access (we will set scale to flip it often)
+onready var sprite = $sprite
+
+func _physics_process(delta):
+	#increment counters
+
+	onair_time += delta
+	shoot_time += delta
+
+	### MOVEMENT ###
+
+	# Apply Gravity
+	linear_vel += delta * GRAVITY_VEC
+	# Move and Slide
+	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
+	# Detect Floor
+	if is_on_floor():
+		onair_time = 0
+
+	on_floor = onair_time < MIN_ONAIR_TIME
+
+	### CONTROL ###
+
+	# Horizontal Movement
+	var target_speed = 0
+	if Input.is_action_pressed("move_left"):
+		target_speed += -1
+	if Input.is_action_pressed("move_right"):
+		target_speed +=  1
+
+	target_speed *= WALK_SPEED
+	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
+
+	# Jumping
+	if on_floor and Input.is_action_just_pressed("jump"):
+		linear_vel.y = -JUMP_SPEED
+		$sound_jump.play()
